a0b8e256acf82a5277f167d1cd3b00b06c55a1d8
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-05-02 18:23:03 +0100

    focus totally working

--- lib/ThirdTab.dart
+++ lib/ThirdTab.dart
@@ -1,144 +1,215 @@
+import 'dart:ui';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 void main() => runApp(const ThirdTab());
 
-class ThirdTab extends StatelessWidget {
-  const ThirdTab({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Circular Countdown Timer Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: const MyHomePage(title: 'Circular Countdown Timer'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
+class ThirdTab extends StatefulWidget {
+  const ThirdTab({Key? key, this.title}) : super(key: key);
 
   final String? title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ThirdTabState createState() => _ThirdTabState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final int _duration = 10;
+class _ThirdTabState extends State<ThirdTab> {
   final CountDownController _controller = CountDownController();
+  int duration = 10;
+  int total_time = 0;
+
+  void changeDuration(String str) {
+    setState(() {
+      debugPrint(str + "hello");
+      duration = int.parse(str);
+      debugPrint("Set duration to $duration");
+    });
+  }
+
+  void changeTotalTime() {
+    setState(() {
+      total_time = total_time + duration;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Center(
-          child: CircularCountDownTimer(
-        // Countdown duration in Seconds.
-        duration: _duration,
-
-        // Countdown initial elapsed Duration in Seconds.
-        initialDuration: 0,
-
-        // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
-        controller: _controller,
-
-        // Width of the Countdown Widget.
-        width: MediaQuery.of(context).size.width / 2,
-
-        // Height of the Countdown Widget.
-        height: MediaQuery.of(context).size.height / 2,
-
-        // Ring Color for Countdown Widget.
-        ringColor: Colors.grey[300]!,
-
-        // Ring Gradient for Countdown Widget.
-        ringGradient: null,
-
-        // Filling Color for Countdown Widget.
-        fillColor: Colors.purpleAccent[100]!,
-
-        // Filling Gradient for Countdown Widget.
-        fillGradient: null,
-
-        // Background Color for Countdown Widget.
-        backgroundColor: Colors.purple[500],
-
-        // Background Gradient for Countdown Widget.
-        backgroundGradient: null,
-
-        // Border Thickness of the Countdown Ring.
-        strokeWidth: 20.0,
-
-        // Begin and end contours with a flat edge and no extension.
-        strokeCap: StrokeCap.round,
-
-        // Text Style for Countdown Text.
-        textStyle: const TextStyle(
-          fontSize: 33.0,
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
+    debugPrint("middle of the code: $duration");
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Circular Countdown Timer Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
         ),
+        home: Card(
+            child: Column(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(top: 50, bottom: 50),
+              child: Flexible(
+                  flex: 1,
+                  child: Text("Focus",
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold)))),
+          Flexible(
+              flex: 2,
+              child: Center(
+                  child: CircularCountDownTimer(
+                // Countdown duration in Seconds.
+                duration: duration,
 
-        // Format for the Countdown Text.
-        textFormat: CountdownTextFormat.S,
+                // Countdown initial elapsed Duration in Seconds.
+                initialDuration: 0,
 
-        // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
-        isReverse: false,
+                // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
+                controller: _controller,
 
-        // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
-        isReverseAnimation: false,
+                // Width of the Countdown Widget.
+                width: MediaQuery.of(context).size.width / 2,
 
-        // Handles visibility of the Countdown Text.
-        isTimerTextShown: true,
+                // Height of the Countdown Widget.
+                height: MediaQuery.of(context).size.height / 2,
 
-        // Handles the timer start.
-        autoStart: false,
+                // Ring Color for Countdown Widget.
+                ringColor: Colors.grey[300]!,
 
-        // This Callback will execute when the Countdown Starts.
-        onStart: () {
-          // Here, do whatever you want
-          debugPrint('Countdown Started');
-        },
+                // Ring Gradient for Countdown Widget.
+                ringGradient: null,
 
-        // This Callback will execute when the Countdown Ends.
-        onComplete: () {
-          // Here, do whatever you want
-          debugPrint('Countdown Ended');
-        },
-      )),
-      Container(
-          padding: EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              // floatingActionButton: Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              const SizedBox(
-                width: 30,
-              ),
-              _button(title: "Start", onPressed: () => _controller.start()),
-              const SizedBox(
-                width: 10,
-              ),
-              _button(title: "Pause", onPressed: () => _controller.pause()),
-              const SizedBox(
-                width: 10,
-              ),
-              _button(title: "Resume", onPressed: () => _controller.resume()),
-              const SizedBox(
-                width: 10,
-              ),
-              _button(
-                  title: "Restart",
-                  onPressed: () => _controller.restart(duration: _duration))
-            ],
-          ))
-    ]);
+                // Filling Color for Countdown Widget.
+                fillColor: Color.fromARGB(255, 119, 188, 172)!,
+
+                // Filling Gradient for Countdown Widget.
+                fillGradient: null,
+
+                // Background Color for Countdown Widget.
+                backgroundColor: Colors.teal[500],
+
+                // Background Gradient for Countdown Widget.
+                backgroundGradient: null,
+
+                // Border Thickness of the Countdown Ring.
+                strokeWidth: 20.0,
+
+                // Begin and end contours with a flat edge and no extension.
+                strokeCap: StrokeCap.round,
+
+                // Text Style for Countdown Text.
+                textStyle: const TextStyle(
+                  fontSize: 33.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+
+                // Format for the Countdown Text.
+                textFormat: CountdownTextFormat.S,
+
+                // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+                isReverse: true,
+
+                // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+                isReverseAnimation: false,
+
+                // Handles visibility of the Countdown Text.
+                isTimerTextShown: true,
+
+                // Handles the timer start.
+                autoStart: false,
+
+                // This Callback will execute when the Countdown Starts.
+                onStart: () {
+                  // Here, do whatever you want
+                  debugPrint('Countdown Started');
+                },
+
+                // This Callback will execute when the Countdown Ends.
+                onComplete: () {
+                  // Here, do whatever you want
+                  debugPrint('Countdown Ended');
+                  changeTotalTime();
+                },
+              ))),
+          Flexible(
+              flex: 1,
+              child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Flexible(
+                          flex: 1,
+                          child: Text(
+                            "How much time do you want to focus? (mins)",
+                            style: TextStyle(color: Colors.black, fontSize: 14),
+                          )),
+                      Flexible(
+                          flex: 1,
+                          child: TextField(
+                            inputFormatters: [],
+                            keyboardType: TextInputType.number,
+                            decoration: new InputDecoration(
+                              icon: Icon(Icons.assessment),
+                              hintText: "Minutes",
+                            ),
+                            onChanged: (String str) {
+                              changeDuration(str);
+                            },
+                          ))
+                    ],
+                  ))),
+          Flexible(
+              flex: 1,
+              child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      // floatingActionButton: Row(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: [
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      _button(
+                          title: "Start",
+                          onPressed: () =>
+                              _controller.restart(duration: duration)),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      _button(
+                          title: "Pause", onPressed: () => _controller.pause()),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      _button(
+                          title: "Resume",
+                          onPressed: () => _controller.resume()),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      // _button(
+                      //     title: "Restart",
+                      //     onPressed: () =>
+                      //         _controller.restart(duration: duration))
+                    ],
+                  ))),
+          Flexible(
+              flex: 1,
+              child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "Total Time Focused: ",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(total_time.toString() + " mins")
+                    ],
+                  )))
+        ])));
   }
 
   Widget _button({required String title, VoidCallback? onPressed}) {
@@ -149,7 +220,7 @@ class _MyHomePageState extends State<MyHomePage> {
         style: const TextStyle(color: Colors.white),
       ),
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.purple),
+        backgroundColor: MaterialStateProperty.all(Colors.teal[500]),
       ),
       onPressed: onPressed,
     ));
