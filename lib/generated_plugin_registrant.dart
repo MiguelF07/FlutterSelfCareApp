a0d0d4647f500d31768408c35eca7fe71b2660da
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-05-04 19:26:43 +0100

    changes

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,7 +6,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:camera_web/camera_web.dart';
-import 'package:geolocator_web/geolocator_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:location_web/location_web.dart';
 
@@ -15,7 +14,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   CameraPlugin.registerWith(registrar);
-  GeolocatorPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
